7aab0a81d58c4da91e0309b131af355031f01dee
Alexzambre <anbari-alexandre@live.fr>
2025-01-08 18:50:52 +0100

    menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // Détecte si la page actuelle est dans le dossier "pages"
+  // Vérifie si la page actuelle est dans le dossier "pages"
   const isInPagesFolder = window.location.pathname.includes("/pages/");
 
+  // Définir les chemins dynamiques en fonction de la page courante
   const menuHTML = `
     <nav>
       <ul>
-        <li><a href="${isInPagesFolder ? '../' : ''}index.html">Accueil</a></li>
-        <li><a href="${isInPagesFolder ? '' : 'pages/'}prestations.html">Prestations</a></li>
-        <li><a href="${isInPagesFolder ? '' : 'pages/'}reservation.html">Réservations</a></li>
+        <li><a href="${isInPagesFolder ? '../index.html' : 'index.html'}">Accueil</a></li>
+        <li><a href="${isInPagesFolder ? 'prestations.html' : 'pages/prestations.html'}">Prestations</a></li>
+        <li><a href="${isInPagesFolder ? 'reservation.html' : 'pages/reservation.html'}">Réservations</a></li>
       </ul>
     </nav>
   `;
 
+  // Injecte le menu dans le conteneur <div id="menu">
   document.getElementById("menu").innerHTML = menuHTML;
 });
