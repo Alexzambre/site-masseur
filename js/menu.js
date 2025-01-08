00d9b54a988119663550ce8f18b5bb3c236bb381
Alexzambre <anbari-alexandre@live.fr>
2025-01-08 18:42:42 +0100

    menu.js

--- js/menu.js
+++ js/menu.js
@@ -3,8 +3,8 @@ document.addEventListener("DOMContentLoaded", () => {
     <nav>
       <ul>
         <li><a href="../index.html">Accueil</a></li>
-        <li><a href="prestations.html">Prestations</a></li>
-        <li><a href="reservation.html">Réservations</a></li>
+        <li><a href="pages/prestations.html">Prestations</a></li>
+        <li><a href="pages/reservation.html">Réservations</a></li>
       </ul>
     </nav>
   `;
