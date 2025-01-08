9a3f1da3d48e728692d2f46b4d55f6b0cae780c0
Alexzambre <anbari-alexandre@live.fr>
2025-01-08 18:24:36 +0100

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const menuHTML = `
+    <nav>
+      <ul>
+        <li><a href="../index.html">Accueil</a></li>
+        <li><a href="prestations.html">Prestations</a></li>
+        <li><a href="reservation.html">Réservations</a></li>
+      </ul>
+    </nav>
+  `;
+  document.getElementById("menu").innerHTML = menuHTML;
+});
