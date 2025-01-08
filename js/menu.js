a4bf77624d529182c33f73b76f04a4715f57b4f2
Alexzambre <anbari-alexandre@live.fr>
2025-01-08 18:44:22 +0100

    menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // Détecte si la page actuelle est dans le dossier "pages"
+  const isInPagesFolder = window.location.pathname.includes("/pages/");
+
   const menuHTML = `
     <nav>
       <ul>
-        <li><a href="../index.html">Accueil</a></li>
-        <li><a href="pages/prestations.html">Prestations</a></li>
-        <li><a href="pages/reservation.html">Réservations</a></li>
+        <li><a href="${isInPagesFolder ? '../' : ''}index.html">Accueil</a></li>
+        <li><a href="${isInPagesFolder ? '' : 'pages/'}prestations.html">Prestations</a></li>
+        <li><a href="${isInPagesFolder ? '' : 'pages/'}reservation.html">Réservations</a></li>
       </ul>
     </nav>
   `;
