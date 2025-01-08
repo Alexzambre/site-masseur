8245cb066fcd2421cb41ddaad0c108e22f2121d6
Alexzambre <anbari-alexandre@live.fr>
2025-01-08 18:56:27 +0100

    menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,33 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // Vérifie si la page actuelle est dans le dossier "pages"
-  const isInPagesFolder = window.location.pathname.includes("/pages/");
+  // Détermine la profondeur de la page en comptant les "/"
+  const pathDepth = window.location.pathname.split('/').length - 1;
 
-  let menuHTML;
+  // Calcule le préfixe pour remonter correctement dans l'arborescence
+  const prefix = '../'.repeat(pathDepth - 1);
 
-  if (isInPagesFolder) {
-    // Si la page est dans le dossier "pages"
-    menuHTML = `
-      <nav>
-        <ul>
-          <li><a href="../index.html">Accueil</a></li> <!-- Remonte au niveau racine -->
-          <li><a href="prestations.html">Prestations</a></li> <!-- Même dossier -->
-          <li><a href="reservation.html">Réservations</a></li> <!-- Même dossier -->
-        </ul>
-      </nav>
-    `;
-  } else {
-    // Si la page est à la racine
-    menuHTML = `
-      <nav>
-        <ul>
-          <li><a href="index.html">Accueil</a></li> <!-- Reste au niveau racine -->
-          <li><a href="pages/prestations.html">Prestations</a></li> <!-- Dossier "pages" -->
-          <li><a href="pages/reservation.html">Réservations</a></li> <!-- Dossier "pages" -->
-        </ul>
-      </nav>
-    `;
-  }
+  // Génère le menu avec les chemins dynamiques
+  const menuHTML = `
+    <nav>
+      <ul>
+        <li><a href="${prefix}index.html">Accueil</a></li>
+        <li><a href="${prefix}pages/prestations.html">Prestations</a></li>
+        <li><a href="${prefix}pages/reservation.html">Réservations</a></li>
+      </ul>
+    </nav>
+  `;
 
-  // Injecte le menu dans le conteneur <div id="menu">
+  // Injecte le menu dans l'élément <div id="menu">
   document.getElementById("menu").innerHTML = menuHTML;
 });
