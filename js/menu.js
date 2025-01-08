08761158a7c0dd7c0bbe9b692f1f2d687dc1f5d4
Alexzambre <anbari-alexandre@live.fr>
2025-01-08 18:41:00 +0100

    menu.js

--- js/menu.js
+++ js/menu.js
@@ -2,11 +2,12 @@ document.addEventListener("DOMContentLoaded", () => {
   const menuHTML = `
     <nav>
       <ul>
-        <li><a href="index.html">Accueil</a></li>
-        <li><a href="pages/prestations.html">Prestations</a></li>
-        <li><a href="pages/reservation.html">Réservations</a></li>
+        <li><a href="../index.html">Accueil</a></li>
+        <li><a href="prestations.html">Prestations</a></li>
+        <li><a href="reservation.html">Réservations</a></li>
       </ul>
     </nav>
   `;
+
   document.getElementById("menu").innerHTML = menuHTML;
 });
